9203153b820b2b2f7ec9e70af4e759a033a36a8c
SNelson <snelson1337@gmail.com>
2019-09-18 20:40:00 -0500

    Added View Products for manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,17 +26,33 @@ connection.connect(function (err) {
 });
 
 
-function manageProducts(){
+function manageProducts() {
     inquirer.prompt(
         {
-            name:"options",
-            message:"Select a menu option:",
-            type:"list",
-            choices:[`View Products for Sale`, `View Low Inventory`,
-        `Add to Inventory`, `Add New Product`]   
+            name: "options",
+            message: "Select a menu option:",
+            type: "list",
+            choices: [`View Products for Sale`, `View Low Inventory`,
+                `Add to Inventory`, `Add New Product`]
         }
-    ).then(function(answer){
+    ).then(function (answer) {
         console.log(answer.options);
+        if (answer.options === `View Products for Sale`) {
+            connection.query("SELECT * FROM products", function (err, res) {
+                if (err) throw err;
+                const table = new AsciiTable('Bamazon Products')
+                table.setHeading(`Item ID`, `Product Name`, `Price`, `Stock Quantity`);
+                for (let i = 0; i < res.length; i++) {
+                    let item_id = res[i].item_id;
+                    let product_name = res[i].product_name;
+                    let price = `$${res[i].price}`;
+                    let stock_quantity = res[i].stock_quantity;
+                    table.addRow(item_id, product_name, price, stock_quantity);
+                }
+                console.log(table.toString());
+            });
+
+        }
         connection.end();
     });
 }
