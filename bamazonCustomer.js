5387daad61287bf9fc488c52e18f70111789c3e9
SNelson <snelson1337@gmail.com>
2019-09-18 20:33:46 -0500

    Added manager inquirer and logged answer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@
 
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const AsciiTable = require('ascii-table')
+const AsciiTable = require('ascii-table');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -81,41 +81,3 @@ function purchaseProducts() {
     });
 }
 
-function createProduct() {
-    console.log("Inserting a new product...\n");
-    var query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-            flavor: "Rocky Road",
-            price: 3.0,
-            quantity: 50
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " product inserted!\n");
-            // Call updateProduct AFTER the INSERT completes
-            updateProduct();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
-}
-
-
-
-function deleteProduct() {
-    console.log("Deleting all strawberry icecream...\n");
-    connection.query(
-        "DELETE FROM products WHERE ?",
-        {
-            flavor: "strawberry"
-        },
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " products deleted!\n");
-            // Call readProducts AFTER the DELETE completes
-            readProducts();
-        }
-    );
-}
---
+++ bamazonManager.js
@@ -0,0 +1,42 @@
+
+//Our Requires from either Node or NPM
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const AsciiTable = require('ascii-table');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazonDB"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    manageProducts();
+});
+
+
+function manageProducts(){
+    inquirer.prompt(
+        {
+            name:"options",
+            message:"Select a menu option:",
+            type:"list",
+            choices:[`View Products for Sale`, `View Low Inventory`,
+        `Add to Inventory`, `Add New Product`]   
+        }
+    ).then(function(answer){
+        console.log(answer.options);
+        connection.end();
+    });
+}
